6361549b79bf46758121be6e5f8e2799193f77e3
developmentUser <43236513+developmentUser@users.noreply.github.com>
2018-10-08 17:43:11 +0200

    Update testProd.js

--- css/testProd.js
+++ css/testProd.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
   $contents = $(".notes > .note > .row > .content.col-sm-9").removeClass("col-sm-9").addClass("col-sm-12");
   console.log("content found: " + $contents.length);
   
+  /*
   formContext.data.addOnLoad(function(){
     $contents2 = $(".notes > .note > .row > .content.col-sm-9").removeClass("col-sm-9").addClass("col-sm-12");
     console.log("content2 found: " + $contents2.length);
   });
+  */
 });
