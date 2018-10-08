7d945db1783cfd020991dca1cf9266f0f8b0c5cf
developmentUser <43236513+developmentUser@users.noreply.github.com>
2018-10-08 17:20:51 +0200

    Update testProd.js

--- css/testProd.js
+++ css/testProd.js
@@ -1,3 +1,4 @@
+<script src="../ClientGlobalContext.js.aspx" type="text/javascript" ></script>
 $(document).ready(function(){
   $contents = $(".notes > .note > .row > .content.col-sm-9").removeClass("col-sm-9").addClass("col-sm-12");
   console.log("content found: " + $contents.length);
