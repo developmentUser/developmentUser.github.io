df5050734f910bed1f5c9983fced6a7a8a043170
developmentUser <43236513+developmentUser@users.noreply.github.com>
2018-10-08 16:55:43 +0200

    Update testProd.js

--- css/testProd.js
+++ css/testProd.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){
-  $(".notes > .note > .row > .content.col-sm-9").removeClass("col-sm-9").addClass("col-sm-12");
+  $contents = $(".notes > .note > .row > .content.col-sm-9").removeClass("col-sm-9").addClass("col-sm-12");
+  console.log("content found: " + $contents.length);
 });
